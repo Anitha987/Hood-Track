9d5e294f1eecf13fa7cf14817ce1774f6657ae94
Anitha987 <anithaumuhire@gmail.com>
2019-11-01 14:41:32 +0200

    add model claclass

--- track/models.py
+++ track/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from tinymce.models import HTMLField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Neighbour(models.Model):
+    name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    occupant= models.CharField(max_length=30)
+
+  def __str__(self):
+      return self.name
+
+class Business(models.Models):
+    name = models.CharField(max_length=30)
+    neighbour = models.ForeignKey(user)
+
