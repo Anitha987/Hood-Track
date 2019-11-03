ea983aab18021273c8e8e9c8b8b1efe5e4762baa
Anitha987 <anithaumuhire@gmail.com>
2019-11-03 18:07:23 +0200

    add neighbour

--- track/admin.py
+++ track/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Business,Neighbour,Profile
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Neighbour)
